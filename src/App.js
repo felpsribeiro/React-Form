e591ae12445e959e78eaadd08bbce3423d243aa7
Felipe N Ribeiro <felipe.ribeiro@vr.com.br>
2022-02-09 14:38:20 -0300

    feat: component Formulario

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import { Fragment } from 'react/cjs/react.production.min';
 import './App.css';
+import Formulario from './components/Formulario.jsx';
 
 function App() {
   return (
-    <h1>Formulário</h1>
+    <Fragment>
+      <h1>Formulário</h1>
+      <Formulario/>
+    </Fragment>
   );
 }
 
